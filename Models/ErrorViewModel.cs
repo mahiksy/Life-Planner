3a151f44f5ad3f91247dc1da6455770e90c3cf60
cyndicact <114026080+cyndicact@users.noreply.github.com>
2022-12-10 15:38:03 -0500

    deleted duplicate files

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace cis4951.sprint.w41._4.Models
-{
-     public class ErrorViewModel
-     {
-          public string? RequestId { get; set; }
-
-          public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-     }
-}
